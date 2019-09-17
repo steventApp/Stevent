15b8ae046efac2c496b466cfb4dd887a4680be83
indriislp <37954242+indriislp@users.noreply.github.com>
2019-09-17 09:12:49 +0700

    Delete Hapus_Event.php

--- PSPEC Code/Hapus_Event.php
+++
@@ -1,9 +0,0 @@
-<?php
-session start();
-include 'koneksi.php';
-$event_id = $_GET['event_id'];
-
-mysql_query("DELETE FROM event WHERE event_id='$event_id')or die(mysql_error());
-
-header("location:index.php?pesan=hapus");
-?>
