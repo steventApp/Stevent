c009b86e255f05c175d7afe1dbce582c85475dc7
Muhammad Alfhi Saputra <alfhisa@student.telkomuniversity.ac.id>
2019-12-12 04:33:06 +0700

    Update TicketsTest.php

--- Testing/Automated Testing/tests/Unit/TicketsTest.php
+++ Testing/Automated Testing/tests/Unit/TicketsTest.php
@@ -18,11 +18,29 @@ class TicketsTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function user_can_buy_ticket(){
-
+     public function user_can_buy_ticket(){
+        $ticket = factory(Tickets::class)->event([
+            'id_event'    => 'test',
+        ]);
+        $response = $this->belongsTo('App\Events','id_event');
+        $this->seePageIs('/my-ticket');
     }
-
     public function user_can_see_ticket_they_buy(){
-        
+        $response = $this->json('GET', '/api/products');
+            $response->assertStatus(200);
+            $response->assertJsonStructure(
+                [
+                    [
+                            'id_event',
+                            'judul',
+                            'tempat',
+                            'deskripsi',
+                            'id_user',
+                            'harga',
+                            'jadwal',
+                            'sampul'
+                    ]
+                ]
+            );
     }
 }
